856037b45602570482a28075ae8c1fc64dc5a9a6
Jose Cebreros <123668616+JRCEGA@users.noreply.github.com>
2024-04-23 18:49:27 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,9 +32,9 @@ const MiPagina = () => {
       )}
       <div className={showSplash ? 'hidden-content' : ''}>
         <div className="mainHeader">
-          <Link href="/">
+          <link href="/">
             <h1 id="mainHeaderTitle">Based Soldiers</h1>
-          </Link>
+          </link>
           <div className="walletConnectButton">
             <ConnectButton />
           </div>
