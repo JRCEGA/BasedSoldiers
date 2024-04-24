e5b9c587d25603fe4d2387c42f88628799872088
JRCEGA <robertoce1030@gmail.com>
2024-04-23 18:37:05 -0700

    First Commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,58 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@rainbow-me/rainbowkit/styles.css';
+import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  Locale,
+  darkTheme,
+  getDefaultConfig,
+} from '@rainbow-me/rainbowkit';
+import {
+  argentWallet,
+  trustWallet,
+  ledgerWallet,
+} from '@rainbow-me/rainbowkit/wallets';
+import { WagmiProvider } from 'wagmi';
+import {
+  base
+} from 'wagmi/chains';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const { wallets } = getDefaultWallets();
+
+const config = getDefaultConfig({
+  appName: 'Based Soldiers',
+  projectId: '8261c63280dd335f76c0502e938e4342',
+  wallets: [
+    ...wallets,
+    {
+      groupName: 'Other',
+      wallets: [argentWallet, trustWallet, ledgerWallet],
+    },
+  ],
+  chains: [
+    base,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [base] : []),
+  ],
+  ssr: true,
+});
+
+const queryClient = new QueryClient();
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const { locale } = useRouter() as { locale: Locale };
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider modalSize="compact" theme={darkTheme()} locale={locale}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
+
+export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Based Soldiers</title>
+        <link rel="icon" href="/BasedSoldiers.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
