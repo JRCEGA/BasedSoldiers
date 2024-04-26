2434caf698dccb362058a246569398295145e73d
Jose Cebreros <123668616+JRCEGA@users.noreply.github.com>
2024-04-26 00:39:48 -0700

    About Deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,6 @@ import Image from "next/image";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useEffect, useState } from 'react';
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import Link from "next/link";
-import dynamic from 'next/dynamic';
 
 const MiPagina = () => {
   const [showSplash, setShowSplash] = useState(true);
@@ -97,7 +95,22 @@ const MiPagina = () => {
               </div>
               <div id="separationBar"></div>
               <div className="contentScreen">
-                <span></span>
+                <h1>WHO ARE WE?</h1>
+                <div id="separationBar2"></div>
+                <br />
+                <p>
+                  Before we start. I want to thank the web designer and the art creator for making this idea possible and achieving it with me. With that said. lets begin by saying that BasedSoldiers is an idea that emerged two years ago. Back then. it wasnt pixel art. and it wasnt on the Base network as it didn't even exist. The art was of soldiers. and we even uploaded it to OpenSea. but there was no success at that time. Among the group of friends. the idea of releasing the sol-diet NFTs always persisted.
+                  <br className="br"/>
+                  <br />
+                  Time moved on. and we acquired enough knowledge to develop the project property two years Later. Our team consists of three people:
+                  <br className="br"/>
+                  Two of us studied in the computing/ technology field
+                  <br className="br"/>
+                  and one is a high school- student.
+                  <br className="br"/>
+                  <br />
+                  The developer has three years of experience in cryptos. the web designer has two years of experience with cryptos. and the art director has no experience in crypto. just drawing experience. so I made a special- request for this collection. The purpose of the collection is purely to share the idea and the art that was developed at one point; we want to bring together people who appreciate NFTs and see value beyond just an image. We believe we have a concept that is different from existing ones and think we can gather a community with this collection. We hope you can enjoy it as much as we enjoyed developing the idea.
+                </p>
               </div>
             </div>
           </div>
@@ -123,5 +136,5 @@ const MiPagina = () => {
   );
 };
 
-export default dynamic (() => Promise.resolve(MiPagina), { ssr: false });
+export default MiPagina;
 
